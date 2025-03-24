82f781eb77c61aadf70189a971a94933a22ced99
barboraferencova <136703767+barboraferencova@users.noreply.github.com>
2025-03-24 10:19:38 +0100

    working fetch api without user input and showing results on website

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const button = document.getElementById("search-button");
 const input = document.getElementById("city-input");
+const apiKey = "e40cbf15fd022fd8a0e3dcb6fc083079"
 
-button.addEventListener("click", function () {
-    console.log("Kliknuto");
-    console.log(input.value);
+// button.addEventListener("click", function () {
+//     console.log("Kliknuto");
+//     console.log(input.value);
     
-})
+// })
+
+const city = "Prague";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const cityName = data.name;
+      const temp = data.main.temp;
+      const description = data.weather[0].description;
+
+    console.log(`Počasí v ${cityName}:`);
+    console.log(`Teplota: ${temp}°C`);
+    console.log(`Popis: ${description}`);
+    })
+    .catch(error => {
+      console.error("Chyba při načítání dat:", error);
+    });
