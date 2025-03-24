f50f30e332c9a634c1a091c891c2765a8811fb18
barboraferencova <136703767+barboraferencova@users.noreply.github.com>
2025-03-24 09:27:46 +0100

    Working eventListener

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+
+button.addEventListener("click", function () {
+    console.log("Kliknuto");
+    console.log(input.value);
+    
+})
