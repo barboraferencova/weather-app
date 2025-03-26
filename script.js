4c376266ef5416e92831d83c97c91ff4974d23e1
barboraferencova <ferencovab@seznam.cz>
2025-03-26 12:05:37 +0100

    Showing weather info response on page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const button = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
 const apiKey = "e40cbf15fd022fd8a0e3dcb6fc083079";
+const weatherInfo = document.querySelector(".weather-info");
 
 function getWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -18,9 +19,12 @@ function getWeather(city) {
       const temp = data.main.temp;
       const description = data.weather[0].description;
 
-      console.log(`Počasí v ${cityName}:`);
-      console.log(`Teplota: ${temp}°C`);
-      console.log(`Popis: ${description}`);
+      weatherInfo.innerHTML = 
+      `
+      <h2> Weather in: ${cityName}</h2>
+      <p> Temperature: ${temp} </p>
+      <p> ${description}</p>
+      `
     })
     .catch(error => {
       console.error("Error - data not loaded:", error);
